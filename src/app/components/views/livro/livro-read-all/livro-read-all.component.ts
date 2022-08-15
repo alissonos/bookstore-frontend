8350220506795022ae4e7b65b061ca159a776b6f
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-08-15 12:32:54 -0300

    Livro Create Component

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Livro } from "../livro.model";
 import { LivroService } from "../livro.service";
 
@@ -15,17 +15,25 @@ export class LivroReadAllComponent implements OnInit {
 
   livros: Livro[] = [];
 
-  constructor(private service: LivroService, private route: ActivatedRoute) {}
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
     this.findAll();
   }
 
   findAll(): void {
     this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
-      this.livros = resposta; 
+      this.livros = resposta;
       console.log(this.livros);
     });
+  }
+
+  navegarParaCriarLivro(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`])
   }
 }
